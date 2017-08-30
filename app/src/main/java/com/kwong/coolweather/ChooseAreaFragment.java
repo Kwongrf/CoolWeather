4054be7b5a5d8e3919ee818eb58c5c01a223234d
kuangrf <kuangruifeng@bupt.edu.cn>
2017-08-30 21:06:11 +0800

    version 1.0 可以查询天气

--- app/src/main/java/com/kwong/coolweather/ChooseAreaFragment.java
+++ app/src/main/java/com/kwong/coolweather/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.kwong.coolweather;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -17,6 +18,7 @@ import android.widget.Toast;
 import com.kwong.coolweather.db.City;
 import com.kwong.coolweather.db.County;
 import com.kwong.coolweather.db.Province;
+import com.kwong.coolweather.gson.Weather;
 import com.kwong.coolweather.util.HttpUtil;
 import com.kwong.coolweather.util.Utility;
 
@@ -95,6 +97,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
--- app/src/main/java/com/kwong/coolweather/util/Utility.java
+++ app/src/main/java/com/kwong/coolweather/util/Utility.java
@@ -2,9 +2,11 @@ package com.kwong.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.google.gson.Gson;
 import com.kwong.coolweather.db.City;
 import com.kwong.coolweather.db.County;
 import com.kwong.coolweather.db.Province;
+import com.kwong.coolweather.gson.Weather;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -81,5 +83,19 @@ public class Utility {
             }
         }
         return false;
+    }
+    /**
+     * 将返回的JSON数据解析成Weather类
+     */
+    public static Weather handleWeatherResponse(String response){
+        try{
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent,Weather.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 }
